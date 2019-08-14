be7ac5c6d5119fffc984f1e58be26c4ff6519640
Malcolm Carey <malcolmcarey6@gmail.com>
2019-08-13 23:05:47 -0400

    Initial check-in of module scratch

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello cruel world.\n");
+
+            Console.WriteLine("What is your name?");
+            string name_in = Console.ReadLine();
+
+            if (String.Equals("MALCOLM", name_in.ToUpper()))
+            {
+                Console.WriteLine("That's a great name!");
+            }
+            else
+            {
+                Console.WriteLine("Ah {0}, what a shitty name.", name_in);
+            }
+        }
+    }
+}
